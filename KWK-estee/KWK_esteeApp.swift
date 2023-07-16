6c29679ba90d28754d49b4f5b7a777f4a78f836e
Jenny Trexler <jennytrexler@gmail.com>
2023-07-16 01:00:25 -0700

    Initial Commit

---
+++ KWK-estee/KWK_esteeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_esteeApp.swift
+//  KWK-estee
+//
+//  Created by Jenny Trexler on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct KWK_esteeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
